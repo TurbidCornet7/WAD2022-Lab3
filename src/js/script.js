23fe78cbc17084fd2b5fe1999f1083c8c33e564f
TurbidCornet7m <vaartnoukarl@gmail.com>
2022-09-28 17:32:15 +0300

    lab 4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -43,4 +43,14 @@ function compareVariables(var1, var2) {
     }
 }
 
+function fibonacci(number) {
+    let number1 = 0, number2 = 1, nextnumber;
+    let numbers = []
+    for (let i = 0; i < number; i++) {
+        numbers.push(number1)
+        nextnumber = number2 + number1;
+        number1 = number2
+        number2 = nextnumber
+    }
+}
 
